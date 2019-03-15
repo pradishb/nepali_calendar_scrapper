192294dd17083921e2f469023b423e3922c9ff7a
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2019-03-15 14:44:29 +0545

    Adds holidays

--- main.py
+++ main.py
@@ -5,29 +5,31 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 months = ["Baisakh",
-        #   "Jestha",
-        #   "Ashad",
-        #   "Shrawan",
-        #   "Bhadra",
-        #   "Ashwin",
-        #   "Kartik",
-        #   "Mangshir",
-        #   "Poush",
-        #   "Magh",
-        #   "Falgun",
-        #   "Chaitra",
+          "Jestha",
+          "Ashad",
+          "Shrawan",
+          "Bhadra",
+          "Ashwin",
+          "Kartik",
+          "Mangshir",
+          "Poush",
+          "Magh",
+          "Falgun",
+          "Chaitra",
           ]
 
 dashi_l = {}
 nday_l = {}
 eday_l = {}
 fest_l = {}
+holiday_l = {}
 
 for month in months:
     dashi_l[month] = []
     nday_l[month] = []
     eday_l[month] = []
     fest_l[month] = []
+    holiday_l[month] = []
 
     s = urlopen("http://nepalicalendar.rat32.com/index_nep.php?year=2076&month="+month +
                 "&dowhat=nepali-calendar-horoscope-game-download-unicode-new-year-mobile-calendar&view=b86e8d03fe992d1b0e19656875ee557c").read()
@@ -50,8 +52,14 @@ for month in months:
                 if i == 2:
                     if (nday[j+w*7].font.string != None):
                         nday_l[month].append(nday[j+w*7].font.string)
+                        if nday[j+w*7].font["color"] == "red":
+                            holiday_l[month].append("holiday")
+                        else:
+                            holiday_l[month].append("normal")
+
                     else:
                         nday_l[month].append(" ")
+                        holiday_l[month].append("normal")
 
                 if i == 3:
                     if (eday[j+w*7].font.string != None):
@@ -69,4 +77,4 @@ for month in months:
                     else:
                         fest_l[month].append(" ")
 
-xml_writer.write(dashi_l, eday_l, nday_l, fest_l, months)
+xml_writer.write(dashi_l, eday_l, nday_l, fest_l, holiday_l, months)
--- xml_writer.py
+++ xml_writer.py
@@ -1,7 +1,8 @@
 import xml.etree.cElementTree as ET
 import xml.etree.ElementTree as etree
+from copy import copy
 
-def write(dashi,eday,nday,fest,months):
+def write(dashi,eday,nday,fest,holiday,months):
     root = ET.Element("Root")
 
     for month in months:
@@ -11,13 +12,13 @@ def write(dashi,eday,nday,fest,months):
         fest[month].reverse()
 
         nepali_days = [
-            "आईत\nSun",
-            "सोम\nMon",
-            "मंगल\nTue",
-            "बुध\nWed",
-            "बिही\nThu",
-            "शुक्र\nFri",
-            "शनि\nSat"]
+            ("आईत\nSun", "normal"),
+            ("सोम\nMon", "normal"),
+            ("मंगल\nTue", "normal"),
+            ("बुध\nWed", "normal"),
+            ("बिही\nThu", "normal"),
+            ("शुक्र\nFri", "normal"),
+            ("शनि\nSat", "holiday"),]
 
         aid =  'http://ns.adobe.com/AdobeInDesign/4.0/'
         aid5 =  'http://ns.adobe.com/AdobeInDesign/5.0/'
@@ -40,10 +41,12 @@ def write(dashi,eday,nday,fest,months):
                 etree.QName(aid, "crows"):"1",
                 etree.QName(aid, "ccols"):"2",
                 etree.QName(aid5, "cellstyle"):"nheader",
-                }).text = d
+                etree.QName(aid, "cstyle"):d[1],
+                }).text = d[0]
         
         # fest
-        for _ in range(5):
+        for j in range(5):
+            i = 0
             for _ in range(7):
                 ET.SubElement(table, "Cell", {
                     etree.QName(aid, "table"):"cell",
@@ -51,30 +54,42 @@ def write(dashi,eday,nday,fest,months):
                     etree.QName(aid, "ccols"):"1",
                     etree.QName(aid, "ccolwidth"):"10",
                     etree.QName(aid5, "cellstyle"):"fest",
+                    etree.QName(aid, "cstyle"):holiday[month][7*j + i],
                     }).text = fest[month].pop()
                 ET.SubElement(table, "Cell", {
                     etree.QName(aid, "table"):"cell",
                     etree.QName(aid, "crows"):"1",
                     etree.QName(aid, "ccols"):"1",
+                    etree.QName(aid, "ccolwidth"):"42",
                     etree.QName(aid5, "cellstyle"):"dashi",
+                    etree.QName(aid, "cstyle"):holiday[month][7*j + i],
                     }).text = dashi[month].pop()
+                i += 1
+            i = 0
             for _ in range(7):
                 ET.SubElement(table, "Cell", {
                     etree.QName(aid, "table"):"cell",
                     etree.QName(aid, "crows"):"1",
                     etree.QName(aid, "ccols"):"1",
+                    etree.QName(aid, "ccolwidth"):"42",
                     etree.QName(aid5, "cellstyle"):"nday",
+                    etree.QName(aid, "cstyle"):holiday[month][7*j + i],
                     }).text = nday[month].pop()
+                i += 1
+            i = 0
             for _ in range(7):
                 ET.SubElement(table, "Cell", {
                     etree.QName(aid, "table"):"cell",
                     etree.QName(aid, "crows"):"1",
                     etree.QName(aid, "ccols"):"1",
+                    etree.QName(aid, "ccolwidth"):"42",
                     etree.QName(aid5, "cellstyle"):"eday",
+                    etree.QName(aid, "cstyle"):holiday[month][7*j + i],
                     }).text = eday[month].pop()
+                i += 1
 
     
 
 
     tree = ET.ElementTree(root)
-    tree.write("filename.xml",  encoding='UTF-8', xml_declaration=True)
+    tree.write("output.xml",  encoding='UTF-8', xml_declaration=True)
